d06c37bf724db98475d2aaa7a31d279f75d9aa4b
mariazangelova <mariazangelova@gmail.com>
2020-05-10 16:25:11 +0200

    Add two google maps components

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,20 @@
 import React from "react";
 import GoogleMap from "../components/GoogleMap";
+import Map from "../components/Map";
 
-export default function About() {
-  return (
-    <div>
-      <GoogleMap />
-    </div>
-  );
+class About extends React.Component {
+  render() {
+    return (
+      <div style={{ margin: "100px" }}>
+        <Map
+          google={this.props.google}
+          center={{ lat: 18.5204, lng: 73.8567 }}
+          height="300px"
+          zoom={15}
+        />
+      </div>
+    );
+  }
 }
+
+export default About;
