027d8f306a22cf31dfa6a2d358a118a5e52e8ccd
mariazangelova <mariazangelova@gmail.com>
2020-05-27 21:34:15 +0200

    Get user data with stored token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/Navigation/NavBar";
 import HomePage from "./pages/HomePage";
@@ -8,8 +9,14 @@ import AddEvent from "./pages/AddEvent";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProfilePage from "./pages/ProfilePage";
+import { getUserWithStoredToken } from "./store/user/actions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <NavBar />
